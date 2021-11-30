3c2a2b3b310e083147a8685581a8de7ea7dc6d6c
OlehFrazik2001 <68520623+OlehFrazik2001@users.noreply.github.com>
2021-11-30 11:58:44 +0200

    Add files via upload

--- VFRenderPdfRelatedRecords/force-app/main/default/aura/ServicesAura/ServicesAuraController.js
+++ VFRenderPdfRelatedRecords/force-app/main/default/aura/ServicesAura/ServicesAuraController.js
@@ -4,8 +4,8 @@
 	},
 
     handleOpen : function(component, event, helper) {
-        var a = component.get("v.recordTypeName"); // get record type name
-        a = a.replace(' ', '_'); // replace all spaces from record type name to set record type developer name
+        var a = component.get("v.recordTypeName"); // get record type developer name
+        
         var urlEvent = $A.get("e.force:navigateToURL");
         urlEvent.setParams({
           "url": "/apex/ServicePageAura?currentRecordId=" + component.get("v.recordId") + "&currentTypeName=" + a + "&startdate=" + component.get("v.startDate") + "&enddate=" + component.get("v.endDate")
