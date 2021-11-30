2c0a7a3081c21d06ee4f5923c051f8b37a3c9433
OlehFrazik2001 <68520623+OlehFrazik2001@users.noreply.github.com>
2021-11-30 11:07:10 +0200

    Add files via upload

--- VFRenderPdfRelatedRecords/force-app/main/default/aura/ServicesAura/ServicesAuraController.js
+++ VFRenderPdfRelatedRecords/force-app/main/default/aura/ServicesAura/ServicesAuraController.js
@@ -1,15 +1,15 @@
 ({
     doInit : function(component, event, helper) {
-		helper.recordTypeHelper(component);
+		helper.recordTypeHelper(component); // get all record types
 	},
 
     handleOpen : function(component, event, helper) {
-        var a = component.get("v.recordTypeName");
-        a = a.replace(' ', '_');
+        var a = component.get("v.recordTypeName"); // get record type name
+        a = a.replace(' ', '_'); // replace all spaces from record type name to set record type developer name
         var urlEvent = $A.get("e.force:navigateToURL");
         urlEvent.setParams({
           "url": "/apex/ServicePageAura?currentRecordId=" + component.get("v.recordId") + "&currentTypeName=" + a + "&startdate=" + component.get("v.startDate") + "&enddate=" + component.get("v.endDate")
-        });
+        }); // pass all parameters from component to url
         urlEvent.fire();
     },
 })
--- VFRenderPdfRelatedRecords/force-app/main/default/aura/ServicesAura/ServicesAuraHelper.js
+++ VFRenderPdfRelatedRecords/force-app/main/default/aura/ServicesAura/ServicesAuraHelper.js
@@ -1,11 +1,11 @@
 ({
     recordTypeHelper : function(component, event, helper) {
-		var action = component.get('c.servicesRecordTypeList');
+		var action = component.get('c.servicesRecordTypeList'); // get methdod of class
         action.setCallback(this, function(response){
             var state = response.getState();      
             if(state == "SUCCESS"){
-                var result = response.getReturnValue();              
-                component.set('v.recordTypeList',result);
+                var result = response.getReturnValue(); // get all record types              
+                component.set('v.recordTypeList',result); //  set all record types to component
             }
         });
         $A.enqueueAction(action);
